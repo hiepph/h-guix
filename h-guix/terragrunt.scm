69279c6339b96e6034a98b87371427d5d7b3a616
Hiep Pham <hiepph9@proton.me>
2024-01-07 00:35:42 +0700

    Bump Terragrunt to 0.54.12

--- h-guix/terragrunt.scm
+++ h-guix/terragrunt.scm
@@ -8,22 +8,22 @@
 (define-public terragrunt
   (package
    (name "terragrunt")
-   (version "0.51.3")
+   (version "0.54.12")
    (source (origin
             (method url-fetch)
             (uri (string-append "https://github.com/gruntwork-io/terragrunt/releases/download/v" version
                                 "/terragrunt_linux_amd64"))
             (sha256
              (base32
-              "17h9jrrrh2b2k32lnhiwsnfn06w9dqj1fl804wh5pbhv8wdkvqvx"))))
+              "0lvxp0g4yb85xw8chn5bmlag4wbh7rff7cl49c5pzx2jxvm67zkh"))))
    (build-system copy-build-system)
    (arguments
     `(#:install-plan '(("terragrunt_linux_amd64" "bin/terragrunt"))
       #:phases
       (modify-phases %standard-phases
-        (add-before 'install 'chmod
-            (lambda _
-              (chmod "terragrunt_linux_amd64" #o0755))))))
+                     (add-before 'install 'chmod
+                                 (lambda _
+                                   (chmod "terragrunt_linux_amd64" #o0755))))))
    (synopsis "Terragrunt is a thin wrapper for Terraform that provides extra tools for working with multiple Terraform modules.")
    (description
     "Terragrunt is a thin wrapper for Terraform that provides extra tools for working with multiple Terraform modules.")
